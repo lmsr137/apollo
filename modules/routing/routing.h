ff4b0b62cafd15b2ba5698617b39c5c7ed9d0a24
yollmn <ldc11@mail.ustc.edu.cn>
2021-10-10 16:12:11 +0800

    push

--- modules/routing/routing.h
+++ modules/routing/routing.h
@@ -1,21 +1,3 @@
-/******************************************************************************
- * Copyright 2017 The Apollo Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *****************************************************************************/
-
-#pragma once
-
 #include <memory>
 #include <string>
 #include <vector>
@@ -33,27 +15,12 @@ class Routing {
   // friend class RoutingTestBase;
  public:
   Routing();
-
-  /**
-   * @brief module name
-   */
   std::string Name() const;
 
-  /**
-   * @brief module initialization function
-   * @return initialization status
-   */
-  apollo::common::Status Init();
+    apollo::common::Status Init();
 
-  /**
-   * @brief module start function
-   * @return start status
-   */
-  apollo::common::Status Start();
+    apollo::common::Status Start();
 
-  /**
-   * @brief destructor
-   */
   virtual ~Routing() = default;
 
   bool Process(const std::shared_ptr<RoutingRequest> &routing_request,
@@ -74,6 +41,4 @@ class Routing {
 
   const hdmap::HDMap *hdmap_ = nullptr;
 };
-
-}  // namespace routing
-}  // namespace apollo
+}}
--- modules/routing/routing_component.h
+++ modules/routing/routing_component.h
@@ -1,28 +1,7 @@
-/******************************************************************************
- * Copyright 2018 The Apollo Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *****************************************************************************/
-
-#pragma once
-
 #include <memory>
-
 #include "modules/routing/routing.h"
-
 namespace apollo {
 namespace routing {
-
 class RoutingComponent final
     : public ::apollo::cyber::Component<RoutingRequest> {
  public:
@@ -45,6 +24,4 @@ class RoutingComponent final
 };
 
 CYBER_REGISTER_COMPONENT(RoutingComponent)
-
-}  // namespace routing
-}  // namespace apollo
+}}
